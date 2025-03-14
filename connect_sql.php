b83e4a094256d264f2ca437b7794f05f762eab44
koipix <koi.devt@gmail.com>
2025-03-14 09:22:00 +0800

    param clenup

--- connect_sql.php
+++ connect_sql.php
@@ -1,7 +1,12 @@
 <?php
 //load page even if it fails
+$host = "localhost";
+$user = "root";
+$pass = 'pix123';
+$db = "userdb";
+
 try {
-    $conn = @mysqli_connect("localhost","root", "pix123", "pix");
+    $conn = @mysqli_connect($host, $user, $pass, $db);
 } catch (mysqli_sql_exception $e) {
     error_log("DB connection failed: ".mysqli_connect_error());
     $conn = null;
